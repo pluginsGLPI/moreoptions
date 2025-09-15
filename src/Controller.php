ca8b5c6d0b9ce9fdb9eb9d0c2ce6a5271ea4c7c4
Lainow <texamaxsam61@gmail.com>
2025-09-15 14:18:23 +0200

    Fix phpstan

--- src/Controller.php
+++ src/Controller.php
@@ -130,7 +130,7 @@ class Controller extends CommonDBTM
         }
     }
 
-    public static function addItemGroups($item)
+    public static function addItemGroups($item): void
     {
         $conf = Config::getCurrentConfig();
         if ($conf->fields['is_active'] != 1) {
