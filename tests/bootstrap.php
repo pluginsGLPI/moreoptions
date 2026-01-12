9bf65a2cec63d843b6b5cafa5daf5d5ea142266b
Samuel Launay <107540223+Lainow@users.noreply.github.com>
2026-01-12 09:01:46 +0100

    Add assign task technician to the ticket (#4)
    * Add assign task technician to the ticket
    
    * Apply suggestions from code review
    
    Co-authored-by: Romain B. <8530352+Rom1-B@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    * Add units tests
    
    * Fix units tests
    
    * FIx units tests
    
    ---------
    
    Co-authored-by: Romain B. <8530352+Rom1-B@users.noreply.github.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -38,8 +38,8 @@ use function Safe\define;
 
 define('GLPI_LOG_DIR', __DIR__ . '/files/_logs');
 
-require_once __DIR__ . '/../../../phpunit/GLPITestCase.php';
-require_once __DIR__ . '/../../../phpunit/DbTestCase.php';
+require_once __DIR__ . '/../../../tests/src/GLPITestCase.php';
+require_once __DIR__ . '/../../../tests/src/DbTestCase.php';
 require_once __DIR__ . '/../../../vendor/autoload.php';
 require_once __DIR__ . '/MoreOptionsTestCase.php';
 
